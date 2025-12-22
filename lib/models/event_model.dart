8f32d696187292af726a8a278b47a641b507522b
denizbarcatayin <deniz.barcatayin@sabanciuniv.edu>
2025-12-22 04:49:00 +0300

    Update event_model.dart

--- lib/models/event_model.dart
+++ lib/models/event_model.dart
@@ -1,14 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class EventModel {
+  final String id;
   final String title;
-  final String subtitle;
-  final String date;
-  final String daysLeft;
+  final String description;
+  final String createdBy;
+  final DateTime createdAt;
 
   EventModel({
+    required this.id,
     required this.title,
-    required this.subtitle,
-    required this.date,
-    required this.daysLeft,
+    required this.description,
+    required this.createdBy,
+    required this.createdAt,
   });
-}
 
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'createdBy': createdBy,
+      'createdAt': createdAt,
+    };
+  }
+
+  factory EventModel.fromMap(Map<String, dynamic> map) {
+    return EventModel(
+      id: map['id'] ?? '',
+      title: map['title'] ?? '',
+      description: map['description'] ?? '',
+      createdBy: map['createdBy'] ?? '',
+      createdAt: (map['createdAt'] as Timestamp).toDate(),
+    );
+  }
+}
